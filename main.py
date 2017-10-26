9b5dcdcd3fd139b9ad89364297820f83cc4c6566
joezq <zhouqi.coding@gmail.com>
2017-10-26 22:41:12 +0800

    code completed

--- main.py
+++ main.py
@@ -132,8 +132,6 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
         print("Loss: {}".format(loss))
 
 
-
-
 tests.test_train_nn(train_nn)
 
 def run():
@@ -161,11 +159,21 @@ def run():
 
         # TODO: Build NN using load_vgg, layers, and optimize function
 
+        epochs = 10
+        batch_size = 5
+
+        learning_rate = tf.placeholder(tf.float32)
+        correct_label = tf.placeholder(tf.float32,(None, None, None, num_classes))
+
         # TODO: Train NN using the train_nn function
+        input_image, keep_prob, layer3, layer4,layer7 = load_vgg(sess, vgg_path)
+        nn_last_layer = layers(layer3, layer4, layer7, num_classes)
+        logits, train_op, cross_entropy_loss = optimize(nn_last_layer, correct_label, learning_rate, num_classes)
+        train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image, correct_label, keep_prob, learning_rate)
 
         # TODO: Save inference data using helper.save_inference_samples
         #  helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
-
+        helper.save_inference_samples(runs_dir, data_dir,sess, image_shape, logits, keep_prob, input_image)
         # OPTIONAL: Apply the trained model to a video
 
 
